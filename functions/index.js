4f6a7ca83e58869c7935d988aff4821a6ee87770
braj011 <bhagyaraj01@gmail.com>
2020-08-20 00:45:31 +0100

    Adds user login and signup routes

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,23 @@
 const functions = require('firebase-functions');
 const app = require('express')();
-
 const {
     getAllExercises,
     createExerciseRecord,
     editExerciseRecord,
     deleteExerciseRecord,
 } = require('./APIs/exercises')
+const {
+    loginUser,
+    signUpUser
+} = require('./APIs/users')
+
 
 app.get('/exercises', getAllExercises);
 app.post('/exercises',  createExerciseRecord)
 app.put('/exercises/:exerciseId',  editExerciseRecord)
 app.delete('/exercises/:exerciseId',  deleteExerciseRecord)
+
+// Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
 exports.api = functions.https.onRequest(app);
